746efdd7c981753f98955b610170edd931f81b68
LYuan22 <lucasyuan1528@gmail.com>
2021-10-26 18:36:07 -0500

    adding taser to checkpoint 1-3

--- checkpoint1/src/checkpoint1-3.sql
+++ checkpoint1/src/checkpoint1-3.sql
@@ -1,15 +1,25 @@
 /*
-  % of TRRs involving an officer firearm depending on race?
-
+  % of TRRs involving an officer firearm or taser depending on race?
  */
 
+/*
+ firearm
+ */
 select subject_race,
     count(*) as total_count,
     sum(case when number_of_officers_using_firearm > 0 then 1 else 0 end) as firearm_count,
     (sum(case when number_of_officers_using_firearm > 0 then 1 else 0 end) * 100.0 / count(*) )as firearm_percent
 from trr_trr
+where subject_race is not null
 group by subject_race;
 
 /*
- where number_of_officers_using_firearm > 0
+ taser
  */
+select subject_race,
+    count(*) as total_count,
+    sum(case when taser = 'true' then 1 else 0 end) as taser_count,
+    (sum(case when taser = 'true' then 1 else 0 end) * 100.0 / count(*) )as taser_percent
+from trr_trr
+where subject_race is not null
+group by subject_race;
