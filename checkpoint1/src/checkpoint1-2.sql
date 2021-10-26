a1d6c5cf1992f6ca1907958fb8062282915e3741
LYuan22 <lucasyuan1528@gmail.com>
2021-10-26 17:18:20 -0500

    fixing checkpoint 1-2

--- checkpoint1/src/checkpoint1-2.sql
+++ checkpoint1/src/checkpoint1-2.sql
@@ -2,4 +2,11 @@
  Is there an increased likelihood of a TRR between the race of the officer and race of the subject?
  */
 
-select race from data_officer INNER JOIN trr_trr ON trr_trr.officer_id = data_officer.id
+select race as officer_race,
+       subject_race,
+       count(*) as trr_count,
+       count(*) * 100.0 / (select count(*) from trr_trr where subject_race is not null) as trr_percentage
+from trr_trr
+inner join data_officer on data_officer.id = trr_trr.officer_id
+where subject_race is not null
+group by race, subject_race;
