1a0c419ba3b81b3c3bffa7c5831e9e1a04c2b4c9
Li-Jiren <56675117+Li-Jiren@users.noreply.github.com>
2021-10-27 12:19:55 -0500

    Add files via upload

---
+++ checkpoint1/src/checkpoint1-4.sql
@@ -0,0 +1,17 @@
+select count(data_allegation.most_common_category_id)
+as number_of_force_allegation
+from data_allegation,data_allegationcategory
+where data_allegation.most_common_category_id=data_allegationcategory.id
+AND data_allegationcategory.category='Use Of Force'
+
+select count(*)
+as number_of_trr
+from trr_trr
+
+
+
+
+
+
+
+
