e55d9f8ec10382881f556e1222b51c168371230a
LYuan22 <lucasyuan1528@gmail.com>
2021-10-28 17:12:24 -0500

    adding stuff to checkpoint 1-2

--- checkpoint1/src/checkpoint1-2.sql
+++ checkpoint1/src/checkpoint1-2.sql
@@ -10,3 +10,23 @@ from trr_trr
 inner join data_officer on data_officer.id = trr_trr.officer_id
 where subject_race is not null
 group by race, subject_race;
+
+create temp table allcops as (
+select race as officer_race,
+       (count(data_officer.race) * 1.0),
+       (select count(data_officer.race) from data_officer),
+       ((count(data_officer.race) * 1.0) / (select count(data_officer.race) from data_officer)) as percentage
+from data_officer
+group by race);
+
+create temp table chicago as (
+select data_racepopulation.race as chicago_race,
+       (sum(data_racepopulation.count) * 1.0 / (select sum(data_racepopulation.count) from data_racepopulation inner join data_area on data_racepopulation.area_id = data_area.id where data_area.area_type = 'community')) as percentage
+from data_racepopulation inner join data_area on data_racepopulation.area_id = data_area.id
+where data_area.area_type = 'community'
+group by data_racepopulation.race);
+
+select officer_race,
+       chicago_race,
+       chicago.percentage * allcops.percentage as probability
+from chicago, allcops
