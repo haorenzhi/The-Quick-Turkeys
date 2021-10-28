a57b3f8090d9f10f806094ba3d98f149366aab82
LYuan22 <lucasyuan1528@gmail.com>
2021-10-28 17:23:47 -0500

    updating checkpoint 1-4

--- checkpoint1/src/checkpoint1-4.sql
+++ checkpoint1/src/checkpoint1-4.sql
@@ -1,17 +1,10 @@
 select count(data_allegation.most_common_category_id)
-from data_allegation,data_allegationcategory
 as number_of_force_allegation
+from data_allegation,data_allegationcategory
 where data_allegation.most_common_category_id=data_allegationcategory.id
 AND data_allegationcategory.category='Use Of Force'
 
 select count(*)
 as number_of_trr
 from trr_trr
-
-
-
-
-
-
-
 
